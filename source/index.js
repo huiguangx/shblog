615510ca4f39bc585a1ab92aa6d04942a0981e85
guang <531311582@qq.com>
2024-04-13 15:20:04 +0800

    :sparkles: 添加js文件

---
+++ source/index.js
@@ -0,0 +1,8 @@
+/**
+ * @author        guang <531311582@qq.com>
+ * @date          2024-04-13 15:15:57
+ * Copyright © YourCompanyName All rights reserved
+ */
+const fun = () => {
+  console.log("66666");
+};
