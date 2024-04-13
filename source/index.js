58517eb68a3238628862a5aa32754bc6fe46d8ad
guang <531311582@qq.com>
2024-04-13 15:12:29 +0800

    feat:sparkles:: second commit

---
+++ source/index.js
@@ -0,0 +1,8 @@
+/**
+ * @author        guang <531311582@qq.com>
+ * @date          2024-04-13 15:07:07
+ * Copyright © YourCompanyName All rights reserved
+ */
+const fun = () => {
+  console.log("77777");
+};
